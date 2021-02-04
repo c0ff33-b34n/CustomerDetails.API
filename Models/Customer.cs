4a5a5e3497909994d1639f6fa0363a0ce029e3d0
Stuart Bowes <stuartbowes2000@gmail.com>
2021-02-04 10:29:07 +0000

    Add CustomerDetails model

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerDetails.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        public string BusinessName { get; set; }
+        [StringLength(35)]
+        public string BuildingName { get; set; }
+        [Required, StringLength(35)]
+        public string NumberAndStreet { get; set; }
+        [StringLength(35)]
+        public string LocalityName { get; set; }
+        [Required, StringLength(35)]
+        public string TownOrCity { get; set; }
+        [Required, StringLength(35)]
+        public string County { get; set; }
+        [Required, StringLength(8)]
+        public string PostCode { get; set; }
+    }
+}
